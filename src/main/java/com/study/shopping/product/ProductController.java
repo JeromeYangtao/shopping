d9d6de32186d75a0748e7c719645ca216fd859c2
Thomson <yangtao135@foxmail.com>
2018-10-31 17:03:23 +0800

    增加product restful API

--- src/main/java/com/study/shopping/product/ProductController.java
+++ src/main/java/com/study/shopping/product/ProductController.java
@@ -1,15 +1,42 @@
 package com.study.shopping.product;
 
-import com.study.shopping.product.model.Product;
+import com.study.shopping.product.model.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
-    @GetMapping("/product")
-    public Product index(){
-        return new Product("1","1","2",3);
+
+    /**
+     * List products
+     */
+    @GetMapping("/products")
+    public ResponseEntity<ListProductResponse> listProducts() {
+        return new ResponseEntity<>(new ListProductResponse(), HttpStatus.OK);
     }
-//    public ResponseEntity<ListProductResponse> listProducts() {
-//        return new ResponseEntity<>(new ListProductResponse(), HttpStatus.OK);
-//    }
+
+    /**
+     * Get products
+     */
+    @GetMapping("/products/{productId}")
+    public ResponseEntity<GetProductResponse> getProduct(@PathVariable String productId) {
+        return new ResponseEntity<>(new GetProductResponse(), HttpStatus.OK);
+    }
+
+    /**
+     * Create products
+     */
+    @PostMapping("/products")
+    public ResponseEntity<CreateProductResponse> CreateProduct(@RequestBody CreateProductRequest createProductRequest) {
+        return new ResponseEntity<>(new CreateProductResponse(), HttpStatus.CREATED);
+    }
+
+    /**
+     * Update products
+     */
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<UpdateProductResponse> UpdateProduct(@PathVariable String productId, @RequestBody UpdateProductRequest updateProductRequest) {
+        return new ResponseEntity<>(new UpdateProductResponse(), HttpStatus.OK);
+    }
 }
