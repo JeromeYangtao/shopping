b649da7d71e31a8fb97ce095593ad5d305d1e142
Thomson <yangtao135@foxmail.com>
2018-12-02 01:16:31 +0800

    feat:定义Cart和Order实体

--- src/main/java/com/study/shopping/order/model/Order.java
+++ src/main/java/com/study/shopping/order/model/Order.java
@@ -1,4 +1,38 @@
 package com.study.shopping.order.model;
 
+//订单
 public class Order {
+    private Integer id;
+    private String status;
+    private String address;
+
+    public Order(Integer id, String status, String address) {
+        this.id = id;
+        this.status = status;
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
